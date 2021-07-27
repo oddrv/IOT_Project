e34806182896f1d784f739ba72221746aa410f93
CristianPlaz <capc@azc.uam.mx>
2021-07-26 23:19:34 -0500

    sigue al conejo blanco

--- despierta.c
+++ despierta.c
@@ -3,7 +3,7 @@
 
 int main()
 {
-    printf( "El universo es una creación mental sostenida en la mente del todo." );
+    printf( "El universo es una creación mental sostenida en la mente del todo.." );
     
 
     getch(); /* Pausa */
